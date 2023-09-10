e8de653363000fc841eb84e4842f589485b5f5bc
AlexeyRybicky <Ranangariel@gmail.com>
2023-09-11 01:14:58 +0300

    Refactoring Добавление виджета TEXT Отображение данных файлов в виджете TEXT

--- UI/UI.py
+++ UI/UI.py
@@ -1,69 +1,68 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
+from tkinter.scrolledtext import ScrolledText
 
 
-
-class App(tk.Tk):
+class App:
     def __init__(self):
-        super().__init__()
-        icon = tk.PhotoImage(file='4711243-200.png')
-        self.title('Моя база данных')
-        self.iconphoto(False, icon)
-        self.geometry('1200x720+100+0')
-        self.minsize(800, 600)
-        self['background'] = '#f2f6d0'
+        self.root = Tk()
+        title = 'Моя база данных'
+        self.root.title(title)
+        self.root.geometry('1200x720+100+0')
+        self.root.minsize(800, 600)
+        self.text = ScrolledText(self.root)
 
+    def draw_menu(self):
+        menu_bar = Menu(self.root)
+        self.root.configure(menu=menu_bar)
 
-class Main_menu(tk.Frame):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        menu_bar = tk.Menu(self.master)
-        self.master.config(menu=menu_bar)
-        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu = Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label='Файл', underline=0,
                              menu=file_menu)
 
-        sub_file_menu = tk.Menu(file_menu, tearoff=0)
+        sub_file_menu = Menu(file_menu, tearoff=0)
         sub_file_menu.add_command(label='Excel',
                                   command=self.open_file)
         sub_file_menu.add_command(label='CSV',
                                   command=self.open_file)
         sub_file_menu.add_command(label='TXT',
                                   command=self.open_file)
-
         file_menu.add_cascade(label='Импортировать',
                               menu=sub_file_menu,
                               underline=0)
+
         file_menu.add_cascade(label='Сохранить',
                               command=self.save_file)
 
         file_menu.add_separator()
-        file_menu.add_command(label='Выход', command=self.onExit)
+
+        file_menu.add_command(label='Выход',
+                              command=self.on_exit)
 
     def open_file(self):
-        name = fd.askopenfilename()
-        print(name)
+        file_name = fd.askopenfilename()
+        if file_name:
+            with open(file_name, 'r') as f:
+                self.text.insert(END, f.read())
 
     def save_file(self):
         name = fd.asksaveasfilename()
-        print(name)
 
-    def onExit(self):
+    def on_exit(self):
         choice = mb.askyesno('Выход', 'Вы действительно хотите выйти?')
         if choice:
-            self.quit()
+            self.root.quit()
+
+    def draw_widgets(self):
+        self.draw_menu()
+        self.text.pack()
+
+    def run(self):
+        self.draw_widgets()
+        self.root.mainloop()
 
 
-
-
-
-
-
-
-app = App()
-menu = Main_menu()
-app.mainloop()
+if __name__ == '__main__':
+    app = App()
+    app.run()
